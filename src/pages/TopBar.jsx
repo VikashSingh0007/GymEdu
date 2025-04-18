f6fdf497bf49ca1c9f2f82735b6f866afd4b1381
Ram <ramnewasgkp@gmail.com>
2025-04-19 04:39:52 +0530

    added

--- src/pages/TopBar.jsx
+++ src/pages/TopBar.jsx
@@ -7,21 +7,21 @@ const TopBar = ({ contactInfo, socialLinks }) => {
     <div className="py-2 text-sm text-white shadow-md bg-gradient-to-r from-indigo-800 via-purple-800 to-pink-800">
       <div className="container flex items-center justify-between px-4 mx-auto sm:px-6 lg:px-8">
         {/* Contact Info */}
-        <div className="flex items-center space-x-4 sm:space-x-6">
+        <div className="flex items-center space-x-4">
           {/* Phone */}
           <div className="flex items-center space-x-1">
             <FaPhoneAlt className="w-4 h-4 text-white" aria-hidden="true" />
             <span className="font-medium">{contactInfo.phone}</span>
           </div>
           {/* Email */}
-          <div className="flex items-center space-x-1">
+          <div className="items-center hidden space-x-1 sm:flex">
             <MdEmail className="w-5 h-5 text-white" aria-hidden="true" />
             <span className="font-medium">{contactInfo.email}</span>
           </div>
         </div>
 
         {/* Social Icons */}
-        <div className="flex items-center space-x-4 sm:space-x-6">
+        <div className="flex items-center space-x-4">
           {socialLinks.map((social) => (
             <a
               key={social.name}
